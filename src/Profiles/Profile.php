6823a7b45787e16c448ea12e1373c30f50bc4b2e
Freek Van der Herten <freek@spatie.be>
2018-02-17 21:11:20 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Profiles/Profile.php
+++ src/Profiles/Profile.php
@@ -42,7 +42,6 @@ abstract class Profile
     {
         $this->directives['report-uri'] = [$uri];
 
-
         $reportToContents = json_encode([
             'url' => $uri,
             'group-name' => class_basename(static::class),
@@ -61,7 +60,7 @@ abstract class Profile
 
     public function addNonceForDirective(string $directive): self
     {
-        return $this->addDirective($directive, "'nonce-" . app('csp-nonce') . "'");
+        return $this->addDirective($directive, "'nonce-".app('csp-nonce')."'");
     }
 
     public function applyTo(Response $response)
