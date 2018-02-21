bf01bbf7f2a6de059007a1d34e97cd90f0270022
TVke <thomas_verhelst@telenet.be>
2018-02-21 17:06:04 +0100

    improve basic profile

--- src/Policies/Basic.php
+++ src/Policies/Basic.php
@@ -9,14 +9,15 @@ class Basic extends Policy
     public function configure()
     {
         $this
+            ->addDirective(Directive::BASE, 'self')
             ->addDirective(Directive::CONNECT, 'self')
             ->addDirective(Directive::DEFAULT, 'self')
             ->addDirective(Directive::FORM_ACTION, 'self')
             ->addDirective(Directive::IMG, 'self')
             ->addDirective(Directive::MEDIA, 'self')
+            ->addDirective(Directive::OBJECT, 'none')
             ->addDirective(Directive::SCRIPT, 'self')
             ->addDirective(Directive::STYLE, 'self')
-            ->addDirective(Directive::OBJECT, 'none')
             ->addNonceForDirective(Directive::SCRIPT)
             ->addNonceForDirective(Directive::STYLE);
     }
