ce926c6e5bda63f5ca90886e6c106741b4920738
Tom Witkowski <dev.gummibeer@gmail.com>
2018-04-11 22:52:56 +0200

    #25 - use value constants in basic policy

--- src/Policies/Basic.php
+++ src/Policies/Basic.php
@@ -9,15 +9,15 @@ class Basic extends Policy
     public function configure()
     {
         $this
-            ->addDirective(Directive::BASE, 'self')
-            ->addDirective(Directive::CONNECT, 'self')
-            ->addDirective(Directive::DEFAULT, 'self')
-            ->addDirective(Directive::FORM_ACTION, 'self')
-            ->addDirective(Directive::IMG, 'self')
-            ->addDirective(Directive::MEDIA, 'self')
-            ->addDirective(Directive::OBJECT, 'none')
-            ->addDirective(Directive::SCRIPT, 'self')
-            ->addDirective(Directive::STYLE, 'self')
+            ->addDirective(Directive::BASE, Directive::VALUE_SELF)
+            ->addDirective(Directive::CONNECT, Directive::VALUE_SELF)
+            ->addDirective(Directive::DEFAULT, Directive::VALUE_SELF)
+            ->addDirective(Directive::FORM_ACTION, Directive::VALUE_SELF)
+            ->addDirective(Directive::IMG, Directive::VALUE_SELF)
+            ->addDirective(Directive::MEDIA, Directive::VALUE_SELF)
+            ->addDirective(Directive::OBJECT, Directive::VALUE_NONE)
+            ->addDirective(Directive::SCRIPT, Directive::VALUE_SELF)
+            ->addDirective(Directive::STYLE, Directive::VALUE_SELF)
             ->addNonceForDirective(Directive::SCRIPT)
             ->addNonceForDirective(Directive::STYLE);
     }
