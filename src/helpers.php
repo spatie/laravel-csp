4338698247f6a2970d7fcd1ea92c0160f645ba53
MegaXLR <weijgsvgjsrogigjeciorscjgioecsjrgoic@megaxlr.net>
2019-01-28 14:09:45 +0100

    add csp_attr shortcut
    Signed-off-by: MegaXLR <weijgsvgjsrogigjeciorscjgioecsjrgoic@megaxlr.net>

--- src/helpers.php
+++ src/helpers.php
@@ -6,3 +6,13 @@ if (! function_exists('csp_nonce')) {
         return app('csp-nonce');
     }
 }
+
+
+if (! function_exists('csp_attr')) {
+    function csp_attr(): \Illuminate\Support\HtmlString
+    {
+            $nonce = csp_nonce();
+            return new \Illuminate\Support\HtmlString(sprintf('nonce="%s"', e($nonce)));
+
+    }
+}
