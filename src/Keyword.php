cdbc7f08561a60ea49479b8ba04aff8e658b350d
Oanh Nguyen <oanhnn.bk@gmail.com>
2020-10-01 06:44:12 +0700

    Add unsafe-hashes keywork (#76)
    See https://content-security-policy.com/unsafe-hashes/

--- src/Keyword.php
+++ src/Keyword.php
@@ -9,5 +9,6 @@ abstract class Keyword
     const SELF = 'self';
     const STRICT_DYNAMIC = 'strict-dynamic';
     const UNSAFE_EVAL = 'unsafe-eval';
+    const UNSAFE_HASHES = 'unsafe-hashes';
     const UNSAFE_INLINE = 'unsafe-inline';
 }
