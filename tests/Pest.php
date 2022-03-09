52af78cfa48c8e4d4ff1bd9060bfc462a640c253
Magiczne <Magiczne@users.noreply.github.com>
2022-03-09 21:04:34 +0000

    Fix styling

--- tests/Pest.php
+++ tests/Pest.php
@@ -4,7 +4,7 @@ use Spatie\Csp\Tests\TestCase;
 
 uses(TestCase::class)->in(__DIR__);
 
-function withoutExceptionHandling(): void 
+function withoutExceptionHandling(): void
 {
     test()->withoutExceptionHandling();
 }
