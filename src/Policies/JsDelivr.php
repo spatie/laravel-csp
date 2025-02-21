0a5622b8aa5f7bbcc20c84e37c9c4423b22dc52b
sebastiandedeyne <sebastiandedeyne@gmail.com>
2025-02-21 13:55:06 +0100

    Add more policies

---
+++ src/Policies/JsDelivr.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Spatie\Csp\Policies;
+
+use Spatie\Csp\Directive;
+
+class JsDelivr extends Policy
+{
+
+    public function configure(): void
+    {
+        $this
+            ->addDirective(Directive::SCRIPT, 'cdn.jsdelivr.net')
+            ->addDirective(Directive::STYLE, 'cdn.jsdelivr.net')
+            ->addDirective(Directive::FONT, 'cdn.jsdelivr.net');
+    }
+}
