942c11d5acd4bd1d92bfa42efd00bd40ebaa85f7
sebastiandedeyne <sebastiandedeyne@gmail.com>
2025-02-21 13:18:05 +0100

    Update required PHP & Laravel versions

--- tests/Policies/GoogleFontsPolicy.php
+++ tests/Policies/GoogleFontsPolicy.php
@@ -9,4 +9,4 @@ it('stringifies GoogleFontsPolicy', function (): void {
     $policy->configure();
 
     expect((string) $policy)->toMatchSnapshot();
-})->skip(version_compare(phpversion(), '8.2.0') === -1, 'Pest snapshot tests not working on earlier versions');
+});
