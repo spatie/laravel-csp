181e360c033407116b3235f8b64a3e288d89d2be
sebastiandedeyne <sebastiandedeyne@gmail.com>
2025-02-25 16:54:48 +0100

    Update wording

--- config/csp.php
+++ config/csp.php
@@ -35,7 +35,7 @@ return [
     'nonce_generator' => Spatie\Csp\Nonce\RandomString::class,
 
     /*
-     * Set to false to disable automatic nonce generation and handling.
+     * Set false to disable automatic nonce generation and handling.
      * This is useful when you want to use 'unsafe-inline' for scripts/styles
      * and cannot add inline nonces.
      * Note that this will make your CSP policy less secure.
