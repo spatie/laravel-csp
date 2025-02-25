62faeefd17681fbf71cbfb2b71d344d485082543
sebastiandedeyne <sebastiandedeyne@gmail.com>
2025-02-25 20:46:59 +0100

    Use data provider for snapshot tests

--- tests/PresetTest.php
+++ tests/PresetTest.php
@@ -3,8 +3,17 @@
 use Spatie\Csp\Policy;
 use Spatie\Csp\Presets;
 
-/** @var class-string<\Spatie\Csp\Preset>[] $presets */
-$presets = [
+it('stringifies', function ($presetClass): void {
+    config(['csp.nonce_enabled' => false]);
+
+    $policy = new Policy();
+
+    $preset = new $presetClass;
+    $preset->configure($policy);
+
+    expect(str_replace(';', PHP_EOL, $policy->getContents()))
+        ->toMatchSnapshot();
+})->with([
     Presets\AdobeFonts::class,
     Presets\Basic::class,
     Presets\Fathom::class,
@@ -13,18 +22,4 @@ $presets = [
     Presets\HubSpot::class,
     Presets\JsDelivr::class,
     Presets\Tolt::class,
-];
-
-foreach ($presets as $presetClass) {
-    it('stringifies ' . $presetClass, function () use ($presetClass): void {
-        config(['csp.nonce_enabled' => false]);
-
-        $policy = new Policy();
-
-        $preset = new $presetClass;
-        $preset->configure($policy);
-
-        expect(str_replace(';', PHP_EOL, $policy->getContents()))
-            ->toMatchSnapshot();
-    });
-}
+]);
