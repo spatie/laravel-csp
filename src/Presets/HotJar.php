b1143ec075b1fb1502de2acefa508d17ebf0bf9c
Jamie <ziadoz@gmail.com>
2025-04-24 17:22:54 +0100

    hotjar preset

---
+++ src/Presets/HotJar.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Spatie\Csp\Presets;
+
+use Spatie\Csp\Directive;
+use Spatie\Csp\Policy;
+use Spatie\Csp\Preset;
+
+class HotJar implements Preset
+{
+    // @see: https://help.hotjar.com/hc/en-us/articles/115011640307-Content-Security-Policies
+    public function configure(Policy $policy): void
+    {
+        $policy
+            ->add(Directive::FRAME, '*.hotjar.com')
+            ->add(Directive::SCRIPT, '*.hotjar.com')
+            ->add(Directive::CONNECT, ['https://*.hotjar.com', 'https://*.hotjar.io', 'wss://*.hotjar.com']);
+    }
+}
