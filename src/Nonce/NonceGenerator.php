c70e568c813f2f105ce23c6c5a74420585217694
Yasser Bal <yasserbal@icloud.com>
2025-06-06 07:20:16 +0200

    Docs | NonceGenerator contract now indicates that the returned string should be a base64-value

--- src/Nonce/NonceGenerator.php
+++ src/Nonce/NonceGenerator.php
@@ -4,5 +4,13 @@ namespace Spatie\Csp\Nonce;
 
 interface NonceGenerator
 {
+    /**
+     * Generates a CSP nonce string.
+     *
+     * The nonce should be a base64-value derived from at least 16 bytes of
+     * cryptographically secure random data, safe for use in HTML attributes and HTTP headers.
+     *
+     * @see https://www.w3.org/TR/CSP3/#grammardef-base64-value
+     */
     public function generate(): string;
 }
