3bc1d146ca168f4125eb386c1a83e73ee0f4da63
Nessim Abadi <nessimabadi@gmail.com>
2025-10-07 15:46:29 -0500

    Add `GoogleMaps` CSP preset

---
+++ src/Presets/GoogleMaps.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Spatie\Csp\Presets;
+
+use Spatie\Csp\Directive;
+use Spatie\Csp\Policy;
+use Spatie\Csp\Preset;
+
+class GoogleMaps implements Preset
+{
+    public function configure(Policy $policy): void
+    {
+        $policy->add(Directive::SCRIPT, [
+            'https://*.googleapis.com',
+            'https://*.gstatic.com',
+            '*.google.com',
+            'https://*.ggpht.com',
+            '*.googleusercontent.com',
+            'blob:',
+        ]);
+        $policy->add(Directive::IMG, [
+            'https://*.googleapis.com',
+            'https://*.gstatic.com',
+            '*.google.com',
+            '*.googleusercontent.com',
+            'data:'
+        ]);
+        $policy->add(Directive::FRAME, '*.google.com');
+        $policy->add(Directive::CONNECT, [
+            'https://*.googleapis.com',
+            '*.google.com',
+            'https://*.gstatic.com',
+            'data:',
+            'blob:',
+        ]);
+        $policy->add(Directive::FONT, 'https://fonts.gstatic.com');
+        $policy->add(Directive::STYLE, 'https://fonts.googleapis.com');
+        $policy->add(Directive::WORKER, 'blob:');
+    }
+}
